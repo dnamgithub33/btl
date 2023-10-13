e9e5335085190404197e54125d461ccafa03eb1a
dnamgithub33 <dangnam.work.study@gmail.com>
2023-10-13 14:42:40 +0700

    #

---
+++ ajax/star-rating-tuto.php
@@ -0,0 +1,29 @@
+<?php
+include "../dbconnect.php";
+session_start();
+if ($_POST) {
+    $customer = $_SESSION['user'];
+    $mediaId = $_POST['mediaId'];
+    $rate = $_POST['rate'];
+    setcookie($customer . $mediaId, 'rated', time() + (86400 * 30), '/');
+    $query = "SELECT rate, nbrrate FROM rating WHERE media='$mediaId'";
+    $result = mysqli_query($con, $query) or die(mysql_error());
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $newRate = $row['rate'] + $rate;
+        $newNbrRate = $row['nbrrate'] + 1;
+        $query = "UPDATE rating SET rate = '$newRate', nbrrate = '$newNbrRate' WHERE media = '$mediaId'";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+
+    } else {
+        $query = "INSERT INTO rating (media, rate, nbrrate) VALUES (' $mediaId', ' $rate ','1')";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+        $newRate = $rate;
+        $newNbrRate = 1;
+    }
+    $average = round($newRate / $newNbrRate, 2);
+    $dataBack = array('avg' => $average, 'nbrRate' => $newNbrRate);
+    $dataBack = json_encode($dataBack);
+    echo $dataBack;
+}
+?>
