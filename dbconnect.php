af348267ba0602a397d9f11d69f5d73ca23bd714
GofN <97667889+dnamgithub33@users.noreply.github.com>
2023-10-17 12:42:25 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 define('DB_HOST', '103.197.185.151');
 define('DB_NAME', 'dangnam1');
 define('DB_USER', 'nam');
-define('DB_PASSWORD', 'G9#X!jUjpw??RAA');
+define('DB_PASSWORD', '');
 
 $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db = mysqli_select_db($con, DB_NAME) or die("Failed to connect to MySQL: " . mysql_error());
